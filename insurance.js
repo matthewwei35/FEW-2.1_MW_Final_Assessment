13b997dbaa26b12785b6fafa4268a4b930e91057
Matthew Wei <matthewwei35@gmail.com>
2021-05-12 15:32:50 -0700

    feat(insurance): complete challenge 1

--- insurance.js
+++ insurance.js
@@ -1,2 +1,23 @@
 var data = require("./data.json");
-console.log(data[1].first_name);
+// Helper Functions //
+function isEmpty(str) {
+    var words = str.trim().split('');
+    for (var i = 0; i < words.length; i += 1) {
+        if (words[i] !== ('\n' || '\r' || '\t')) {
+            return false;
+        }
+    }
+    return true;
+}
+function upperFirst(str) {
+    if (isEmpty(str) === true) {
+        return '';
+    }
+    var strEndSlice = str.slice(1);
+    var firstLetter = str[0].toUpperCase();
+    return firstLetter + strEndSlice;
+}
+function printData() {
+    console.log(upperFirst(data[0].first_name) + " " + upperFirst(data[0].last_name));
+}
+printData();
--- insurance.ts
+++ insurance.ts
@@ -1,3 +1,28 @@
 const data = require("./data.json");
 
-console.log(data[1].first_name)
+// Helper Functions //
+function isEmpty(str: string): boolean {
+  const words: string[] = str.trim().split('');
+  for (let i: number = 0; i < words.length; i += 1) {
+    if (words[i] !== ('\n' || '\r' || '\t')) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function upperFirst(str: string): string {
+  if (isEmpty(str) === true) {
+    return '';
+  }
+  const strEndSlice: string = str.slice(1);
+  const firstLetter: string = str[0].toUpperCase();
+  return firstLetter + strEndSlice;
+}
+
+// Solution //
+function printData() {
+  console.log(`${upperFirst(data[0].first_name)} ${upperFirst(data[0].last_name)}`)
+}
+
+printData()
